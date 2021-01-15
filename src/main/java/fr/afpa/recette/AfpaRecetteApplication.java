65c6b732f5eb1a8bf5cd1c0c691836a89f80761e
Développeur web <51367556+Francois-Legrand@users.noreply.github.com>
2021-01-15 11:09:19 +0100

    first commit

---
+++ src/main/java/fr/afpa/recette/AfpaRecetteApplication.java
@@ -0,0 +1,13 @@
+package fr.afpa.recette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfpaRecetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfpaRecetteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/afpa/recette/AfpaRecetteApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.afpa.recette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfpaRecetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
